25d25fd823296344712ad9ccea55ed59ffda97fe
erantalmor <the.eran.talmor@gmail.com>
2023-04-07 09:51:00 +0300

    Added bottom, prev, and just 1 ctor

--- include/scoped.h
+++ include/scoped.h
@@ -1,32 +1,33 @@
 #ifndef _INCLUDE_SCOPED_H_
 #define _INCLUDE_SCOPED_H_
 
-#include <cstddef>
+#include <utility>
 
 template <class T, class ...Tags> class scoped
 {
 public:
-    scoped() : m_next(s_top) {
+    template <class... Args>
+    scoped(Args&&... args) : m_value{std::forward<Args>(args)...}, m_next(s_top) {
+        if (s_bottom == nullptr) s_bottom = this;
         s_top = this;
-    }
-
-    scoped(const T& data) : m_value(data), m_next(s_top) {
-        s_top = this;
-    }
-
-    scoped(T&& data) : m_value(data), m_next(s_top) {
-        s_top = this;
+        if (m_next) m_next->m_prev = this;
     }
 
     ~scoped() {
+        if (m_next) m_next->m_prev = nullptr;
         s_top = m_next;
+        if (s_bottom == this) s_bottom = nullptr;
     }
 
     T& value() { return m_value; }
  
-    scoped* next() { return m_next; };
+    scoped* next() { return m_next; }
+
+    scoped* prev() { return m_prev; }
     
     static scoped* top() { return s_top; }
+
+    static scoped* bottom() { return s_bottom; }
 
     static T* get() { return s_top ? &(s_top->m_value) : nullptr; }
 
@@ -36,10 +37,13 @@ private:
 
     T m_value;
     scoped* m_next;
+    scoped* m_prev;
 
     static thread_local scoped* s_top;
+    static thread_local scoped* s_bottom;
 };
 
 template<class T, class ...Tags> thread_local scoped<T, Tags...>* scoped<T, Tags...>::s_top;
+template<class T, class ...Tags> thread_local scoped<T, Tags...>* scoped<T, Tags...>::s_bottom;
 
 #endif // _INCLUDE_SCOPED_H_
