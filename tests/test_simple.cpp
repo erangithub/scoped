95d9faa1b257ac325a7681ba1985ba1647ad5dc3
erantalmor <the.eran.talmor@gmail.com>
2023-04-17 21:15:25 +0300

    Removed unneeded include from test_simple

--- tests/test_simple.cpp
+++ tests/test_simple.cpp
@@ -1,5 +1,4 @@
 #include "scoped.h"
-#include <iostream>
 
 using ScopedThreshold = scoped::scoped<int, struct ScopedThresholdTag>;
 
@@ -15,7 +14,7 @@ int get_number(int x) {
 int main() {
     {
         ScopedThreshold scoped_threshold{4};
-        
+
         assert(get_number(3) == 3);   // Expected: The number is 3
         assert(get_number(10) == -1);  // Expected: The number is BIG
     }
